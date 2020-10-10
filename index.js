4082b75f142a99f1b8e14c0f1ad9382671cf1ded
oladeji1993 <olanipekunoladeji282@yahoo.com>
2020-10-10 03:10:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-btn').addEventListener
+('click', () => document.querySelector('.min-menu').classList.toggle('show'));
+
+document.querySelector('.cancel').addEventListener
+('click', () => document.querySelector('.min-menu').classList.toggle('show'));
